cde6823c28a2fb76c1681d70cac3f1acfb15b1ec
amalendu manoj <amalendumanoj@gmail.com>
2022-06-30 12:32:27 +0530

    added'

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
 from tkinter import *
-window=tk()
-window.geometry('250*150')
+window=Tk()
+window.geometry('800x1000')
+entry=Entry(window,textvariable=equation).grid(row='0',column='0')
+number1=Button(window,text='1',height='10',width='10').grid(row='1',column='0')
+number2=Button(window,text='2',height='10',width='10').grid(row='1',column='1')
+number3=Button(window,text='3',height='10',width='10').grid(row='1',column='2')
+number4=Button(window,text='4',height='10',width='10').grid(row='2',column='0')
+number5=Button(window,text='5',height='10',width='10').grid(row='2',column='1')
+number6=Button(window,text='6',height='10',width='10').grid(row='2',column='2')
+number7=Button(window,text='7',height='10',width='10').grid(row='3',column='0')
+number8=Button(window,text='8',height='10',width='10').grid(row='3',column='1')
+number9=Button(window,text='9',height='10',width='10').grid(row='3',column='2')
+numberequal=Button(window,text='=',height='4',width='4').grid(row='3',column='4')
 
 window.mainloop()
